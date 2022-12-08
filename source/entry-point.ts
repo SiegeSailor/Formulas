d4cf07fb5bbaa70cdd3fc530836eabc964796a7a
SiegeSailor <siegesailor@gmail.com>
2022-12-08 18:23:48 -0500

    Commit for workarounds

--- source/entry-point.ts
+++ source/entry-point.ts
@@ -1,3 +1,31 @@
 import figlet from "figlet";
+import { Command } from "commander";
+import { readdirSync } from "fs";
+import { join } from "path";
 
-console.log(figlet.textSync("Dir Manager"));
+import Package from "../package.json";
+
+console.error(figlet.textSync("Formulas", "Banner"));
+
+const program = new Command();
+
+program
+  .name(Package.name)
+  .version(Package.version)
+  .description(Package.description);
+
+program
+  .command("list")
+  .description("List available algorithms.")
+  .action(() => {
+    readdirSync(join(process.cwd(), "algorithms")).forEach((item) => {
+      const isDirectory = item.indexOf(".") == -1;
+      if (isDirectory) return;
+
+      console.log(item);
+    });
+  });
+
+program.parse();
+
+export {};

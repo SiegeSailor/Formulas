619bb31dc09817a966a2e38073d5c75ef99eec81
SiegeSailor <siegesailor@gmail.com>
2022-12-17 16:02:18 -0500

    Modify blum-blum-shub test format

--- source/algorithms/blum-blum-shub/index.test.ts
+++ source/algorithms/blum-blum-shub/index.test.ts
@@ -4,7 +4,9 @@ import { _ as blumBlumShub } from "./index";
 
 describe("Generate pseudo random numbers", () => {
   test.each([1, 5, 10, 20, 30])(
-    `${chalk.greenBright("%p")}-bit random number <= its base-10 value`,
+    `${chalk.greenBright(
+      "%p"
+    )}-bit random number\n\tit is smaller or equal to its base-10 value`,
     (bits) => {
       expect(
         blumBlumShub(bits)() <= BigInt(Math.pow(2, bits) * Math.pow(2, bits))

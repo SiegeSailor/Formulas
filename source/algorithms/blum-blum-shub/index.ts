8cb8bc6088d8dd439e9e637179a7454f78806551
SiegeSailor <siegesailor@gmail.com>
2022-12-20 01:05:14 -0500

    Add prompt for blum-blum-shub

--- source/algorithms/blum-blum-shub/index.ts
+++ source/algorithms/blum-blum-shub/index.ts
@@ -1,6 +1,10 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
 import random from "random-bigint";
 
 export function _(bits: number) {
+  if (bits <= 0) throw new Error("Given bits must be higher than 0.");
+
   const p = random(bits) + BigInt(1),
     q = random(bits) + BigInt(1),
     m = p * q;
@@ -12,4 +16,23 @@ export function _(bits: number) {
     result %= m;
     return result;
   };
+}
+
+export async function prompt() {
+  console.log(`\tgenerate a 8-bit pseudo-random number x. x = result`);
+  console.log(
+    chalk.gray(`\tx is smaller or equal to ${Math.pow(2, 8) * Math.pow(2, 8)}`)
+  );
+
+  const { bits } = await inquirer.prompt([
+    {
+      type: "number",
+      name: "bits",
+      message: `Enter ${chalk.italic("bits")}:`,
+      default: 1,
+    },
+  ]);
+
+  const x = _(bits)();
+  console.log(`\t${bits}-bit x = ${x}`);
 }
